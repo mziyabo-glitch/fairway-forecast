a9a08fbb9727b3300e66918d83196b6e29bae2d2
mziyabo-glitch <mziyabo@gmail.com>
2025-12-15 12:23:49 +0000

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+window.APP_CONFIG = {
+  SUPABASE_URL: "https://bdxgjkhfdrqrcevtdnvw.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJkeGdqa2hmZHJxcmNldnRkbnZ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU1MzQ3ODQsImV4cCI6MjA4MTExMDc4NH0.MAGy3DXobwiGQTgznEnLRtLYdrB6bS48MsYpLqjRdgs",
+  OPENWEATHER_API_KEY: "75e81e064e438b3d07f08396434ebd55",
+  DEFAULT_UNITS: "metric"
+};
