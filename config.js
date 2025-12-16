e69d457d905a953999cd21405b7cf2bc8c85fdc7
mziyabo-glitch <mziyabo@gmail.com>
2025-12-16 22:05:51 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,17 @@
 // config.js
 window.APP_CONFIG = {
-  OWM_API_KEY: "75e81e064e438b3d07f08396434ebd55",
-  SUPABASE_URL: "https://bdxgjkhfdrqrcevtdnvw.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJkeGdqa2hmZHJxcmNldnRkbnZ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU1MzQ3ODQsImV4cCI6MjA4MTExMDc4NH0.MAGy3DXobwiGQTgznEnLRtLYdrB6bS48MsYpLqjRdgs",
+  OPENWEATHER_KEY: "75e81e6e4a3b3d07f80396434ebd55",
+
+  SUPABASE_URL: "https://bdxgikhdfqrcevtddvw.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...",
+
   COURSES_TABLE: "uk_golf_courses",
-  COURSE_COLS: { name: "name", lat: "latitude", lon: "longitude", country: "country" },
+  COURSE_COLS: {
+    name: "name",
+    lat: "latitude",
+    lon: "longitude",
+    country: "country"
+  }
 };
+
+
