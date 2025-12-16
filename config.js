db167457421a8c3b4f61c3ad150800ad2ae872dd
mziyabo-glitch <mziyabo@gmail.com>
2025-12-16 07:37:39 +0000

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+// config.js
+window.APP_CONFIG = {
+  OWM_API_KEY: "75e81e064e438b3d07f08396434ebd55",
+  SUPABASE_URL: "https://bdxgjkhfdrqrcevtdnvw.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJkeGdqa2hmZHJxcmNldnRkbnZ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU1MzQ3ODQsImV4cCI6MjA4MTExMDc4NH0.MAGy3DXobwiGQTgznEnLRtLYdrB6bS48MsYpLqjRdgs",
+  COURSES_TABLE: "uk_golf_courses",
+  COURSE_COLS: { name: "name", lat: "latitude", lon: "longitude", country: "country" },
+};
